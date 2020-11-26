d27fe422c445bab27bb24872dcf5e4957aa67686
JinOptimist <88jin@mail.ru>
2020-11-26 13:49:29 +0300

    AddJs
    Update cool image and add first js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.blok').click(function(){
+		$(this).toggleClass('full-screen');
+	});
+	
+	$('.Для Катя По кому кликать').click(function(){
+		$(this).toggleClass('Какой класс добавить');
+	});
+});

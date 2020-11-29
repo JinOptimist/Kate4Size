8f2b91b0f9b511771a0e52cac97acfacbf1549ae
padawan4 <kotyara-ya@rambler.ru>
2020-11-29 09:40:12 +0300

    gallery

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
 		$(this).toggleClass('full-screen');
 	});
 	
+	$('.gallery-blok').click(function(){
+		$(this).toggleClass('full-screen');
+	});
+	
 	$('.Для Катя По кому кликать').click(function(){
 		$(this).toggleClass('Какой класс добавить');
 	});

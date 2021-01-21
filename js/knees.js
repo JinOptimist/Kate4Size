2531feb11a6f42a48276661534a0105858a44e48
JinOptimist <88jin@mail.ru>
2021-01-21 16:16:45 +0300

    Rename variable. Add click

--- js/knees.js
+++ js/knees.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-		var imageKneesUrls = [
+	var imageKneesUrls = [
 		'image/knees/knees1.jpg',
 		'image/knees/knees2.jpeg',
 		'image/knees/knees3.jpeg',
@@ -7,61 +7,57 @@ $(document).ready(function(){
 		'image/knees/knees5.jpg',
 		'image/knees/knees6.jpg',
 		'image/knees/knees7.jpg',
-		
 	];
+	var indexOfCurrentImage = 0;
 	
 	init();
+	
 	$('.add').click(function(){
-		var namberKithen = $('.namber-knees').text() - 0;
-		if (namberKithen < imageKneesUrls.length - 1){
-			$('.namber-knees').text(namberKithen + 1);
+		if (indexOfCurrentImage < imageKneesUrls.length){
+			indexOfCurrentImage = indexOfCurrentImage + 1;
 		}
-		if (namberKithen == imageKneesUrls.length - 1){
-			$('.namber-knees').text(0);
+		if (indexOfCurrentImage == imageKneesUrls.length){
+			indexOfCurrentImage = 0;
 		}
 		
 		//выполняем каждый раз, как увеличили размер
-		carouselKithen();
+		drawImageAndPreview();
 	});
 	
 	$('.reduct').click(function(){
-		var namberKithen = $('.namber-knees').text() - 0;		
-		if (namberKithen > 0){
-			$('.namber-knees').text(namberKithen - 1);
+		if (indexOfCurrentImage > 0){
+			indexOfCurrentImage = indexOfCurrentImage - 1
 		}
-		if (namberKithen == 0){
-			$('.namber-knees').text(imageKneesUrls.length - 1);
+		if (indexOfCurrentImage == 0){
+			indexOfCurrentImage = imageKneesUrls.length - 1;
 		}
 		//выполняем каждый раз, как уменьшили размер
-		carouselKithen();
+		drawImageAndPreview();
 	});
 	
-	function carouselKithen(){
-		var namberKithen = $('.namber-knees').text() - 0;
-		var url = imageKneesUrls[namberKithen];
+	function drawImageAndPreview(){
+		var url = imageKneesUrls[indexOfCurrentImage];
 		$('.carousel-img').attr('src', url);
-		
-		
 		
 		//пример селектора 
 		//.knees-preview img[knees-id='2']
-		//у селектора мы меняем значение "2" на переменную "namberKithen", 
+		//у селектора мы меняем значение "2" на переменную "indexOfCurrentImage", 
 		//где высчитывается номер картинки
-		var currentImg = $(".knees-preview img[knees-id='" + namberKithen + "']");
-		
+		var currentImg = 
+			$(".knees-preview img[knees-id='" + indexOfCurrentImage + "']");
 		
 		$(".knees-preview img").removeClass('active');
 		currentImg.addClass('active');
-		
-		
 	}
+	
 	function init(){
 		for(var i = 0; i < imageKneesUrls.length; i++){
 			//Эти команды будут выполняться несколько раз
 			createKithen(imageKneesUrls[i], i);
 		}
-		carouselKithen();
+		drawImageAndPreview();
 	}
+	
 	function createKithen(urlForImage, index){
 		//Создаём новый блок (div) которого раньше не было
 		
@@ -69,9 +65,41 @@ $(document).ready(function(){
 		var img = $('<img>');
 		img.attr('src', urlForImage);
 		img.attr("knees-id", index);
+		img.click(onPreviewClick);
 		
 		//Добавили на страницу
 		//Только сейчас пользователь увидит блок и всех его детей
 		$('#knees .knees-preview').append(img);
 	}
+	
+	function onPreviewClick(){
+		var value = $(this).attr('knees-id');
+		
+		indexOfCurrentImage = value - 0;
+		
+		drawImageAndPreview();
+	}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

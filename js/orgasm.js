0bb1592f951d30a07dbbedefcafc167246917f11
padawan4 <kotyara-ya@rambler.ru>
2021-01-24 11:53:25 +0300

    orgasm

--- js/orgasm.js
+++ js/orgasm.js
@@ -1,106 +1,78 @@
 $(document).ready(function(){
-	var imageKneesUrls = [
-		'image/knees/knees1.jpg',
-		'image/knees/knees2.jpeg',
-		'image/knees/knees3.jpeg',
-		'image/knees/knees4.jpg',
-		'image/knees/knees5.jpg',
-		'image/knees/knees6.jpg',
-		'image/knees/knees7.jpg',
+	var imageOrgasmUrls = [
+		'image/orgasm/orgasm1.jpg',
+		'image/orgasm/orgasm2.png',
+		'image/orgasm/orgasm3.jpg',
+		'image/orgasm/orgasm4.jpg',
+		'image/orgasm/orgasm5.jpg',
+		'image/orgasm/orgasm6.jpg',
+		'image/orgasm/orgasm7.jpg',
+		'image/orgasm/orgasm8.jpg',
+		'image/orgasm/orgasm9.jpg',
 	];
 	var indexOfCurrentImage = 0;
 	
 	init();
 	
-	$('.addKnees').click(function(){
-		if (indexOfCurrentImage < imageKneesUrls.length){
+	$('.addOrgasm').click(function(){
+		if (indexOfCurrentImage < imageOrgasmUrls.length){
 			indexOfCurrentImage = indexOfCurrentImage + 1;
 		}
-		if (indexOfCurrentImage == imageKneesUrls.length){
+		if (indexOfCurrentImage == imageOrgasmUrls.length){
 			indexOfCurrentImage = 0;
 		}
 		
 		//выполняем каждый раз, как увеличили размер
 		drawImageAndPreview();
 	});
-	
-	$('.reductKnees').click(function(){		
-		if (indexOfCurrentImage > 0){
+	$('.reductOrgasm').click(function(){
+		if (indexOfCurrentImage >= 0){
 			indexOfCurrentImage = indexOfCurrentImage - 1
 		}
-		if (indexOfCurrentImage == 0){
-			indexOfCurrentImage = imageKneesUrls.length - 1;
+		if (indexOfCurrentImage < 0){
+			indexOfCurrentImage = imageOrgasmUrls.length - 1;
 		}
 		//выполняем каждый раз, как уменьшили размер
 		drawImageAndPreview();
 	});
-	
 	function drawImageAndPreview(){
-		var url = imageKneesUrls[indexOfCurrentImage];
-		//пример селектора #knees .carousel-img (id +стиль)
-		$('#knees .carousel-img').attr('src', url);
+		var url = imageOrgasmUrls[indexOfCurrentImage];
+		$('#orgasm .carousel-img').attr('src', url);
 		
 		//пример селектора 
 		//.knees-preview img[knees-id='2']
 		//у селектора мы меняем значение "2" на переменную "indexOfCurrentImage", 
 		//где высчитывается номер картинки
 		var currentImg = 
-			$(".knees-preview img[knees-id='" + indexOfCurrentImage + "']");
+			$(".orgasm-preview img[orgasm-id='" + indexOfCurrentImage + "']");
 		
-		$(".knees-preview img").removeClass('active');
+		$(".orgasm-preview img").removeClass('active');
 		currentImg.addClass('active');
 	}
-	
 	function init(){
-		for(var i = 0; i < imageKneesUrls.length; i++){
+		for(var i = 0; i < imageOrgasmUrls.length; i++){
 			//Эти команды будут выполняться несколько раз
-			createKithen(imageKneesUrls[i], i);
+			createOrgasm(imageOrgasmUrls[i], i);
 		}
 		drawImageAndPreview();
 	}
-	
-	function createKithen(urlForImage, index){
-		//Создаём новый блок (div) которого раньше не было
-		
+	function createOrgasm(urlForImage, index){
+				
 		//создаём тэг img
 		var img = $('<img>');
 		img.attr('src', urlForImage);
-		img.attr("knees-id", index);
+		img.attr("orgasm-id", index);
 		img.click(onPreviewClick);
 		
 		//Добавили на страницу
 		//Только сейчас пользователь увидит блок и всех его детей
-		$('#knees .knees-preview').append(img);
+		$('#orgasm .orgasm-preview').append(img);
 	}
-	
 	function onPreviewClick(){
-		var value = $(this).attr('knees-id');
+		var value = $(this).attr('orgasm-id');
 		
 		indexOfCurrentImage = value - 0;
 		
 		drawImageAndPreview();
 	}
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

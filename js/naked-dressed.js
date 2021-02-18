877d154ea6a312cfec6ff3223bec081ed5206e01
JinOptimist <88jin@mail.ru>
2021-02-18 16:57:09 +0300

    Example of animate

---
+++ js/naked-dressed.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$('.go').click(function(){
+		$('.red').animate({
+			width: 0
+		}, 2000);
+		
+		$('.green').animate({
+			width: 200
+		}, 2000);
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

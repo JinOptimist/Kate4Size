e363c0621ca11ec58de965d261bc57099f6c5e49
padawan4 <kotyara-ya@rambler.ru>
2021-02-21 19:34:15 +0300

    attr

--- js/reversal.js
+++ js/reversal.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
 	
+	$('.reversal-content').click(function(){
+		
+		var attrSide = $(this).attr("side");
+		
+		
+		if (attrSide == "front"){
+			$('.reversal-img.front').animate({
+				width: 0
+			}, 2000);
+			
+			$('.reversal-img.back').animate({
+				width: 400
+			}, 2000);
+			$(this).attr("side", "back");
+		}
+		if (attrSide == "back"){
+			$('.reversal-img.front').animate({
+				width: 400
+			}, 2000);
+			
+			$('.reversal-img.back').animate({
+				width: 0
+			}, 2000);
+			
+			$(this).attr("side", "front");
+		}
+		
+		
+	});
 });

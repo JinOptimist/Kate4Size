5547a4fa973fb6055bbdb1349b8ce99d22c12921
padawan4 <kotyara-ya@rambler.ru>
2021-03-02 19:43:23 +0300

    animat

--- js/w.js
+++ js/w.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
 	var imageUrlsW = [];
+	var wWidth = 400;
+	var wHeight = 600;
 	
-	for(var index = 1 ; index < 12 ; index++){
-		imageUrlsW.push('image/w/w' + index + '.jpg');
+	var curent = 0;
+	
+	init();
+	
+	function init(){
+		for(var index = 1 ; index < 12 ; index++){
+			imageUrlsW.push('image/w/w' + index + '.jpg');
+		}
+		
+		for(var t = 0; t < imageUrlsW.length; t++){
+			createW(imageUrlsW[t], t);
+		}
+		
+		relaclWidth();
 	}
-	for(var t = 0; t < imageUrlsW.length; t++){
-		createW(imageUrlsW[t], t);
-	}
+	
 	function createW(UrlsForW, index){
 		var divW = $('<div>');
 		divW.addClass('w-content');
@@ -19,6 +31,30 @@ $(document).ready(function(){
 		imgW.attr('src', UrlsForW);
 		
 		$('#w .w-block').append(divW);
-		
-	}	
+	}
+	
+	function relaclWidth(){
+		for(var index = 0 ; index < 12 ; index++){
+
+			var diff= Math.abs(index - curent)  + 1;
+			
+			$("[w-id=" + index + "]").animate({
+				width: wWidth / diff,
+				height: wHeight / diff
+			}, 2000);
+		}
+	}
+	
+	
+	$('#w .w-content').click(onClickAnim);	
+	
+	
+
+	function onClickAnim(){
+		curent = $(this).attr("w-id") -0;
+		relaclWidth();
+	}
+	
+	
+				
 });

ab4a49ccc2838f5769a84d1445e777dbc60ee9f9
JinOptimist <88jin@mail.ru>
2021-03-04 16:21:19 +0300

    Add dinamic image size

--- js/reversal.js
+++ js/reversal.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-	var fullHeight = 600;
-	var fullWidth = 400;
+	// var fullWidth = 400;
+	// var fullHeight = 600;
+	var heightScale = 2 / 3;
 	
-	var currentIndex = 0;
+	var currentIndex = 2;
 	
 	var wifeUrls = [];
 	var imageCount = 12;
@@ -15,13 +16,36 @@ $(document).ready(function(){
 	});
 	
 	function recalcImages(){
-		for(var t = 0; t < wifeUrls.length; t++){//true false
-			var diff = Math.abs(currentIndex -  t) + 1;
+		var containerWidth = $('#reversal .reversal-block').width() - 20;
+		
+		//formula from https://ru.wikipedia.org/wiki/%D0%93%D0%B0%D1%80%D0%BC%D0%BE%D0%BD%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9_%D1%80%D1%8F%D0%B4
+		var fullWidth = (containerWidth - 24 * imageCount) / getEllerSumm();
+		var fullHeight = fullWidth * heightScale;
+		
+		for(var t = 0; t < wifeUrls.length; t++){
+			var diff = getDiff(t);
 			$('.reversal-content[index=' + t + ']').animate({
-				height: fullHeight / diff,
-				width: fullWidth / diff
+				width: fullWidth / diff,
+				height: fullHeight / diff
 			}, 2000);
 		}
+	}
+	
+	function getEllerSumm(){
+		var sum = 0;
+		for(var t = 0; t < wifeUrls.length; t++){
+			var diff = getDiff(t);
+			sum += 1 / diff;
+		}
+		return sum;
+	}
+	
+	function getDiff(index){
+		var diff = Math.abs(currentIndex -  index) + 1;
+		if (diff > 4){
+			diff = 4;
+		}
+		return diff * diff;
 	}
 	
 	function init(){

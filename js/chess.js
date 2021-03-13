78d8afdc03c5a483f5b4729cb906f003743adc16
JinOptimist <88jin@mail.ru>
2021-03-13 12:45:50 +0300

    Add flip function

--- js/chess.js
+++ js/chess.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	var imageUrlsChess = [];
 	
+	var coverImg = 'image/chess/chess7.jpg';
+	
 	init();
 	
 	function init(){
@@ -12,11 +14,9 @@ $(document).ready(function(){
 		for(var t = 0; t < imageUrlsChess.length; t++){
 			createChess(imageUrlsChess[t], t);
 		}
-		
-		
 	}
 	
-	function createChess(UrlsForChess, index){
+	function createChess(urlForChess, index){
 		var divChess = $('<div>');
 		divChess.addClass('chess-content');
 		divChess.attr("chess-id", index);
@@ -27,7 +27,7 @@ $(document).ready(function(){
 		
 		var imgChess = $('<img>');
 		divImgChess.append(imgChess);
-		imgChess.attr('src', UrlsForChess);
+		imgChess.attr('src', coverImg);//urlForChess
 		divChess.append(divImgChess);
 		
 		$('#chess .chess-block').append(divChess);
@@ -35,20 +35,56 @@ $(document).ready(function(){
 	
 	
 	$('.chess-content').click(function(){
+		var nudeGirl = $(this).find('.chess-content-img');
 		
-		var $elem = $('.chess-content-img');
 		
-		$({deg: 0}).animate({deg: 90}, {
-			duration: 2000,
-			step: function(now) {
-            
-            $elem.css({
-                transform: 'rotate(' + now + 'deg)'
-            });
-        }
-    });
 		
+		flip(nudeGirl, 0, 90, backFlip);
+		
+		function backFlip(){
+			var newImage = 
+			nudeGirl.find('img').attr('src', coverImg);
+			flip(nudeGirl, 90, 0);
+		}
 	});
+	
+	function flip(rotateObject, angleFrom, angleTo, onComplete){
+		$({deg: angleFrom}).animate(
+					{deg: angleTo}, 
+					{
+						duration: 2000,
+						step: function(now) {            
+							rotateObject.css({
+								transform: 'rotateY(' + now + 'deg)'
+							});
+						},
+						complete: onComplete
+					}
+		);
+	}
 	
 	
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

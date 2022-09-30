b65195c1754660aecad68a751e9858724ff0635e
padawan4 <kotyara-ya@rambler.ru>
2022-09-30 22:46:59 +0300

    next

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let otvet = 0;
+
+for (let index = 0; index < 10; index++) {
+    let addToOtvet = index + 1;
+    otvet = otvet + addToOtvet;
+    console.log(addToOtvet + ' -> ' + otvet);
+}
+console.log(otvet);
+
+let str = '';
+for (let index = 0; index < 7; index++) {
+    str = str + "   " + (index + 1);
+}
+console.log(str);
+
+let strTwo = '';
+for (let index = 7; index < 14; index++) {
+    strTwo = strTwo + "   " + (index + 1);
+}
+console.log(strTwo);
+
+let strThree = '';
+for (let index = 14; index < 21; index++) {
+    strThree = strThree + "   " + (index + 1);
+}
+console.log(strThree);
+
+let strFour = '';
+for (let index = 21; index < 28; index++) {
+    strFour = strFour + "   " + (index + 1);
+    
+}
+console.log(strFour);
+

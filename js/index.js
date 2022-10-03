efd6362eb55dca0884f8c743278099c1badedb0e
padawan4 <kotyara-ya@rambler.ru>
2022-10-03 10:19:39 +0300

    calendar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let otvet = 0;
 
+//первая задача
 for (let index = 0; index < 10; index++) {
     let addToOtvet = index + 1;
     otvet = otvet + addToOtvet;
@@ -7,28 +8,33 @@ for (let index = 0; index < 10; index++) {
 }
 console.log(otvet);
 
-let str = '';
-for (let index = 0; index < 7; index++) {
-    str = str + "   " + (index + 1);
-}
-console.log(str);
 
-let strTwo = '';
-for (let index = 7; index < 14; index++) {
-    strTwo = strTwo + "   " + (index + 1);
-}
-console.log(strTwo);
+//вторая задача
+for (let row = 0; row < 5; row++) {
+    let str = '';
+    let minDayIndex = row * 7;
+    let maxDayIndex = (row + 1) * 7;
+    if (maxDayIndex > 31) {
+        maxDayIndex = 31;
+    }
+    for (let dayIndex = minDayIndex; dayIndex < maxDayIndex; dayIndex++) {
+        let addionalSpace = '';
+        if (dayIndex < 9) {
+            addionalSpace = ' ';
+        }
 
-let strThree = '';
-for (let index = 14; index < 21; index++) {
-    strThree = strThree + "   " + (index + 1);
+        str = str + "   " + addionalSpace + (dayIndex + 1);
+    }
+    console.log(str);
 }
-console.log(strThree);
 
-let strFour = '';
-for (let index = 21; index < 28; index++) {
-    strFour = strFour + "   " + (index + 1);
-    
-}
-console.log(strFour);
+
+
+
+
+
+
+
+
+
 

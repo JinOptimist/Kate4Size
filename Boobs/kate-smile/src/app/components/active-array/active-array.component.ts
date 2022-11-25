e1b125ab73ae0e854a2bfc64d8daac81fa86665d
JinOptimist <88jin@mail.ru>
2022-11-25 09:04:44 +0300

    Smart click

--- Boobs/kate-smile/src/app/components/active-array/active-array.component.ts
+++ Boobs/kate-smile/src/app/components/active-array/active-array.component.ts
@@ -6,23 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./active-array.component.scss']
 })
 export class ActiveArrayComponent implements OnInit {
-  name: string;
+  newWifeUrl: string;
   wifesUrls: string[];
 
   private index: number = 0;
 
   constructor() {
-    this.name = "Паша";
+    this.newWifeUrl = "/assets/images/Wife00.JPG";
     this.wifesUrls = [];
+
+    this.removeImage('test.jpg');
   }
 
   ngOnInit(): void {
   }
 
-  addImage() {
+  addAutoImage() {
     let newImageUrl = `/assets/images/Wife0${this.index}.JPG`;
     this.wifesUrls.push(newImageUrl);
     this.index++;
   }
 
+  addCustomImage(){
+    this.wifesUrls.push(this.newWifeUrl);
+  }
+
+  removeImage(url: string) {
+    this.wifesUrls = this.wifesUrls.filter(wifeUrl => wifeUrl != url);
+  }
 }
